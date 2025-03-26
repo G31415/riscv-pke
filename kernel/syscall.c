882a2167e5ac414da8b799cf522be562797f3eff
G31415 <1198055805@qq.com>
2025-03-26 03:37:25 +0000

    11

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -270,7 +270,7 @@ long do_syscall(long a0, long a1, long a2, long a3, long a4, long a5, long a6, l
       return sys_user_link((char *)a1, (char *)a2);
     case SYS_user_unlink:
       return sys_user_unlink((char *)a1);
-    // added in lab4_challenge2
+    // added @lab4_challenge2
     case SYS_user_exec:
       return sys_user_exec((char *)a1);
     default:
